7070af564f07f7d3bcdb9fb0bfb349e071c4e4c8
meligera@ya.ru <meligera@ya.ru>
2021-09-13 09:59:47 +0300

    I wish thyself a Happy Birthday

---
+++ 1309/numbers_arraged.py
@@ -0,0 +1,36 @@
+#numbers_ = [5, 2, 12, 1024, 2021, 4, 782, 8970, 7223]
+import random
+'''
+# Ручной способ через функцию
+def sort_(numbers_):
+    for i in range(len(numbers_)):
+        for x in range(i + 1, len(numbers_)):
+            if numbers_[i] > numbers_[x]:
+                numbers_[i], numbers_[x] = numbers_[x], numbers_[i]
+    return numbers_
+'''
+
+'''
+def lenghtMinMax(x):
+    maximum = minimum = x[0]
+    for i in x[1:]:
+        if i < minimum:
+            minimum = i
+        else:
+            if i > maximum: maximum = i
+    return minimum, maximum
+'''
+
+def create_list(lenght, minimum, maximum):
+    list_=[]
+    for i in range (lenght):
+        list_.append(random.randrange(minimum, maximum))
+    return list_
+
+def create_list_short(lenght, minimum, maximum):
+    return [random.randrange(minimum, maximum) for i in range (lenght)]
+
+print(create_list_short(6, 1, 10000))
+
+#print(len(numbers_), lenghtMinMax(numbers_))
+# print(sort_(numbers_))
