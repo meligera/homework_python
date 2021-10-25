9766bcd9b9156a4ae5491b9cf8b359785f5eaf60
meligera@ya.ru <meligera@ya.ru>
2021-10-25 10:06:55 +0300

    matplotlib

---
+++ 2510/mathplot.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as et
+tree = et.ElementTree(file='/home/sirius/homework/2510/menu.xml')
+root = tree.getroot()
+colors = ['r','g', 'b']
+fig = plt.figure()
+plt.ylabel('Prices, $')
+plt.xlabel('Dishes')
+i = 0
+bar_colors = []
+prices = []
+names = []
+for c in root:
+        color = colors[i]
+        for gc in c:
+                bar_colors.append(color)
+                prices.append(float(gc.attrib['price'][1:]))
+                names.append(gc.text)
+        if i == len(colors) - 1:
+                i = 0
+        else:
+                i += 1
+bars = plt.bar(names, prices)
+for i in range (len(bars)):
+        bars[i].set_color(bar_colors[i])
+
+plt.title('Menu prices')
+plt.grid(True)
+plt.show()
--- README.md
+++
@@ -1,3 +0,0 @@
-# homework_python
-Homework but on Python
-Yeah, i agree, it's a comlete mess. We all start somewhere and this is mine. See you, future me!
